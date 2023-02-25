822b9dc98b68e0970ab4888a75008ee4d938eb7e
Rinat F <mylittlebigplanet24@gmail.com>
2023-02-25 18:57:53 +0300

    Добавлен Compose

---
+++ app/build/generated/source/buildConfig/debug/com/example/skills_market/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.skills_market;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.skills_market";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
