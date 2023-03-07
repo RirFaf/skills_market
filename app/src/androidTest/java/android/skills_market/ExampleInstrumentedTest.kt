a7d1d46bddccdb0301d25358598df4a22c70108c
Roman <romakorolev9702@gmail.com>
2023-03-07 17:29:23 +0300

    Добавлен экран профиль

--- app/src/androidTest/java/android/skills_market/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/android/skills_market/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("android.skills_market", appContext.packageName)
+        assertEquals("android.compose", appContext.packageName)
     }
 }
