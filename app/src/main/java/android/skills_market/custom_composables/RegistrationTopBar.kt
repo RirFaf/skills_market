9ae8268a469d94577a409d04830d3d3b07e025ec
Roman <romakorolev9702@gmail.com>
2023-04-18 18:56:35 +0300

    new

--- app/src/main/java/android/skills_market/custom_composables/RegistrationTopBar.kt
+++ app/src/main/java/android/skills_market/custom_composables/RegistrationTopBar.kt
@@ -1,26 +1,73 @@
 package android.skills_market.custom_composables
 
 import android.skills_market.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonElevation
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun RegistrationTopBar() {
-    Row(modifier = Modifier) {
-        Button(onClick = {/*TODO*/ }) {
-            Icon(painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24), contentDescription = "Back Icon")
-            Text(text = "Назад")
+    Box(modifier=Modifier,
+         contentAlignment= Alignment.Center
+    ) {
+
+
+       Row(modifier=Modifier.fillMaxWidth(),
+           horizontalArrangement = Arrangement.Start) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24),
+                contentDescription = "Back Icon",
+                tint = Color.Black
+            )
+
+            Text(
+                text = "Назад",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.SansSerif
+                )
+            )
         }
         Spacer(modifier = Modifier.padding(20.dp))
-        Text(text = "Регистрация")
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center){
+        Text(
+            text = stringResource(id = R.string.registration),
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif
+            )
+
+        )}
     }
 
 }
